3ef985049e27872b82c2a721e84225562ed43003
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-25 16:17:39 +0300

    Get User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarried: boolean;
+  }[] = [
+    { id: 1, name: 'John', age: 28, gender: 'Male', isMarried: false },
+    { id: 2, name: 'Mark', age: 32, gender: 'Male', isMarried: true },
+  ];
+
+  //Returns all Users
+  getAllUsers() {
+    return this.users;
+  }
+  //Returns user based on Id :.find(...)
+}

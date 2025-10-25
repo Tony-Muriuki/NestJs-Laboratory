707b6d388192484f5990069f8e962e41f41a990b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-25 16:37:42 +0300

    Manual Injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   //Get Request--> https://localhost:3000/users
   @Get()
   getUsers() {
-    return 'You made a GET REQUEST';
+    const usersService = new UsersService();
+    usersService.getAllUsers(); //Without Dependency Injection(DI)
   }
 
   // Create User

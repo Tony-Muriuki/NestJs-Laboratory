d74cf3fe5827d0fb7c7727a5b0ba05f8521de2e8
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-28 18:11:39 +0300

    Class Validator

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class createUserDto {
   id: number;
+  @IsNotEmpty()
   @IsString()
+  @MinLength(3)
   name: string;
   @IsString()
   @IsOptional()

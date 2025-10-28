d795e12ce2a75baa245fc833a91177715ad0227b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-28 21:13:50 +0300

    Connecting DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -9,9 +9,9 @@ import {
 
 export class createUserDto {
   id: number;
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Invalid.Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
-  @MinLength(3)
+  @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
   name: string;
   @IsString()
   @IsOptional()

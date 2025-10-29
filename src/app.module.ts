431b85f90c66b68763d2a654219e66af32a89f33
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-29 16:19:33 +0300

    App Module Recorrection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TweetsController } from './tweets/tweets.controller';
 import { TweetsModule } from './tweets/tweets.module';
 // import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TweetsModule, UsersModule], //Registering Other User Defined Modules
-  controllers: [AppController, TweetsController], //Register Contollers
+  controllers: [AppController], //Register Contollers
   providers: [AppService],
 })
 export class AppModule {}

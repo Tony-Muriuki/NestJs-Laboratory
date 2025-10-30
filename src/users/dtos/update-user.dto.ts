1c35f1ec980c1cfb7af683ac2e45c77306a1169c
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-30 16:42:30 +0300

    UpdateUserDto

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsBoolean,
+  IsNumber,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsNumber()
+  id: number;
+  @IsNotEmpty({ message: 'Invalid.Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
+  @IsString()
+  @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @IsOptional()
+  gender: string;
+  @IsBoolean()
+  isMarried: boolean;
+}

b73a4bd3e0c7e356db8eb1c87605b584bb150dbf
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-05 14:01:00 +0300

    Optional Route Param

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 
 @Controller('tweets')
 export class TweetsController {
   constructor(private tweetsService: TweetsService) {}
 
-  @Get()
-  public GetTweets() {}
+  @Get(':userid') // https://localhost:3000/tweets/101
+  public GetTweets(@Param('userid', ParseIntPipe) userid?: number) {
+    console.log(userid);
+  }
 }

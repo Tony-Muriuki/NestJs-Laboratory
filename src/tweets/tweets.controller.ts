f0171e96bee8cb6847fb6740d63d2bdba106cda0
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-05 14:02:03 +0300

    Optional Route Param

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -5,7 +5,7 @@ import { TweetsService } from './tweets.service';
 export class TweetsController {
   constructor(private tweetsService: TweetsService) {}
 
-  @Get(':userid') // https://localhost:3000/tweets/101
+  @Get(':userid') // https://localhost:3000/tweets/101 This get route should get all the tweets which this userid has made.
   public GetTweets(@Param('userid', ParseIntPipe) userid?: number) {
     console.log(userid);
   }

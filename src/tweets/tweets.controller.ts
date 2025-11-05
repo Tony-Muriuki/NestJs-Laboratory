54d927991cf3678fe83462dbc5442f36d04ed631
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-05 13:50:35 +0300

    Get Tweets

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 
 @Controller('tweets')
 export class TweetsController {
   constructor(private tweetsService: TweetsService) {}
+
+  @Get()
+  public GetTweets() {}
 }

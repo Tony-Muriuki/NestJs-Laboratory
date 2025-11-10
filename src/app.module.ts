0f6fd035b9bfbe3d8cc6b65078b002ec4d9726bc
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-10 16:35:25 +0300

    Auth Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
 // import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TweetsModule, UsersModule], //Registering Other User Defined Modules
+  imports: [TweetsModule, UsersModule, AuthModule], //Registering Other User Defined Modules
   controllers: [AppController], //Register Contollers
   providers: [AppService],
 })

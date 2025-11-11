a8cd530fae8608d31ffbc191bcb2de0f8c73a006
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-11 12:04:38 +0300

    Circular Dependency

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  login(email: string, password: string) {
-    return this.authService.login(email, password);
+  @Post()
+  login(@Body() user: { email: string; password: string }) {
+    return this.authService.login(user.email, user.password);
   }
 }

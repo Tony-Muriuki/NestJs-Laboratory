eb1ab7bf8887c1d904261a09098ee1baea40412d
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-11 11:56:09 +0300

    Circular Dependency

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
   login(email: string, pswd: string) {}
 }

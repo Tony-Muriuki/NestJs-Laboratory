226b2fe6b50dd2d5d6c969667cc0100f12dfea72
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-12 17:03:27 +0300

    Asynchronous Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TweetsModule, UsersModule, AuthModule, TypeOrmModule.forRoot()], //Registering Other User Defined Modules
+  imports: [
+    TweetsModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config available everywhere
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ], //Registering Other User Defined Modules
   controllers: [AppController], //Register Contollers
   providers: [AppService],
 })

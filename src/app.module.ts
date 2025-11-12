5ece664b2bfe360f56b2f524b99fc67bada1c25b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-12 12:21:44 +0300

    TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TweetsModule, UsersModule, AuthModule], //Registering Other User Defined Modules
+  imports: [TweetsModule, UsersModule, AuthModule, TypeOrmModule], //Registering Other User Defined Modules
   controllers: [AppController], //Register Contollers
   providers: [AppService],
 })

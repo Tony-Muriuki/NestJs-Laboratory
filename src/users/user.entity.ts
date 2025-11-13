de2a432cba43c91c696c719e0e2abdc97ff30ed8
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-13 21:08:57 +0300

    User Entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,9 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
   @Column()
   firstName: string;
   @Column()

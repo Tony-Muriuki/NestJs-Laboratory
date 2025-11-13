885c57f48316b7609b11c32c6504a69cbd647ead
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-13 18:14:46 +0300

    User DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -7,13 +7,28 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty({ message: 'Invalid.Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
+  @IsNotEmpty({ message: 'Invalid.First Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
   @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
-  name: string;
-  @IsEmail()
-  email: string;
+  firstName: string;
+
+  @IsNotEmpty({ message: 'Invalid. Last Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
+  @IsString()
+  @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
+  lastName: string;
+
   @IsString()
   @IsOptional()
   gender: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, {
+    message: 'Password should have a minimum of Eight charcters',
+  })
+  password: string;
 }

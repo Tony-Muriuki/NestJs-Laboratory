49148ad5f3cdde8eb6254e27bf322b1a120902db
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-13 22:08:28 +0300

    User Entity

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -11,16 +11,22 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'Invalid.First Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
   @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
-  @MaxLength(100)
+  @MaxLength(100, {
+    message: 'First Name should have a maximum of hundred charcters',
+  })
   firstName: string;
 
   @IsNotEmpty({ message: 'Invalid. Last Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
   @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
+  @MaxLength(100, {
+    message: 'Last Name should have a maximum of hundred charcters',
+  })
   lastName: string;
 
   @IsString()
   @IsOptional()
+  @MaxLength(10, { message: 'Gender should have a maximum of Ten characters' })
   gender: string;
 
   @IsEmail()
@@ -29,6 +35,9 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @MaxLength(100, {
+    message: 'Password should have a maximum of 100 characters',
+  })
   @MinLength(8, {
     message: 'Password should have a minimum of Eight charcters',
   })

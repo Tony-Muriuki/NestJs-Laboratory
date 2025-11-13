46413bbeb1c763bd3aa4a3077bbbfac4499cac08
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-13 22:04:29 +0300

    User Entity

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -4,12 +4,14 @@ import {
   IsString,
   MinLength,
   IsOptional,
+  MaxLength,
 } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty({ message: 'Invalid.First Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
   @MinLength(3, { message: 'Name should have a minimum of Three charcters' })
+  @MaxLength(100)
   firstName: string;
 
   @IsNotEmpty({ message: 'Invalid. Last Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -4,7 +4,11 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+  })
   firstName: string;
   @Column()
   lastName: string;

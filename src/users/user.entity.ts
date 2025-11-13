6e9c23e27094d610d94364595ff78a503c12c026
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-13 18:17:19 +0300

    User Entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,15 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class User {
+  @Column()
   firstName: string;
+  @Column()
   lastName: string;
+  @Column()
   gender: string;
+  @Column()
   email: string;
+  @Column()
   password: string;
 }

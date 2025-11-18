69184b8d2c9c89bf77d7d22f3e5fcb72c06605aa
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-18 14:39:33 +0300

    Users Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,86 +8,12 @@ export class UsersService {
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
-  users: {
-    id: number;
-    name: string;
-    email: string;
-    gender: string;
-    isMarried: boolean;
-    password: string;
-  }[] = [
-    {
-      id: 1,
-      name: 'John',
-      email: 'john@gmail.com',
-      gender: 'Male',
-      isMarried: false,
-      password: 'john1234',
-    },
-    {
-      id: 2,
-      name: 'Mark',
-      email: 'mark@gmail.com',
-      gender: 'Male',
-      isMarried: true,
-      password: 'mark2024',
-    },
-    {
-      id: 3,
-      name: 'Alice',
-      email: 'alice@gmail.com',
-      gender: 'Female',
-      isMarried: false,
-      password: 'alice!pass',
-    },
-    {
-      id: 4,
-      name: 'Grace',
-      email: 'grace@gmail.com',
-      gender: 'Female',
-      isMarried: true,
-      password: 'grace789',
-    },
-    {
-      id: 5,
-      name: 'Daniel',
-      email: 'daniel@gmail.com',
-      gender: 'Male',
-      isMarried: true,
-      password: 'daniel321',
-    },
-    {
-      id: 6,
-      name: 'Sophie',
-      email: 'sophie@gmail.com',
-      gender: 'Female',
-      isMarried: false,
-      password: 'sophie@12',
-    },
-    {
-      id: 7,
-      name: 'David',
-      email: 'david@gmail.com',
-      gender: 'Male',
-      isMarried: true,
-      password: 'david007',
-    },
-  ];
 
   //Returns all Users
-  getAllUsers() {
-    if (this.authService.isAuthenticated) {
-      return this.users;
-    }
-    return 'YOU ARE NOT LOGGED IN';
-  }
-  //Returns user based on Id :.find(...)
-  // .find() is a JavaScript array method that goes through each element in the array one by one, and returns the first element that matches a given condition (the “test”).
-  getUserById(id: number) {
-    return this.users.find((x) => x.id === id);
-  }
+  getAllUsers() {}
 
   //Create User
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   createUser(user: {
     id: number;
     name: string;
@@ -95,7 +21,5 @@ export class UsersService {
     gender: string;
     isMarried: boolean;
     password: string;
-  }) {
-    return this.users.push(user);
-  }
+  }) {}
 }

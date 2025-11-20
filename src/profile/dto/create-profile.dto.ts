6ffab79b6c537bb872f2633b39ae597365883cfb
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-21 00:53:38 +0300

    Profile Dto

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -13,7 +13,7 @@ export class CreateProfileDto {
   @MaxLength(100, {
     message: 'First Name should have a maximum of hundred charcters',
   })
-  firstName: string;
+  firstName?: string;
 
   @IsNotEmpty({ message: 'Invalid. Last Name should be string' }) //This validators can also take an error message or thedefault built-in  message will be displayed
   @IsString()
@@ -21,10 +21,10 @@ export class CreateProfileDto {
   @MaxLength(100, {
     message: 'Last Name should have a maximum of hundred charcters',
   })
-  lastName: string;
+  lastName?: string;
 
   @IsString()
   @IsOptional()
   @MaxLength(10, { message: 'Gender should have a maximum of Ten characters' })
-  gender: string;
+  gender?: string;
 }

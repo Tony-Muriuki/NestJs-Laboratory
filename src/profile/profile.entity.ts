ab0d79f9cd6d9a70df4878b4eda57ed4e0fc123c
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-20 23:49:44 +0300

    Profile Entity

--- src/profile/profile.entity.ts
+++ src/profile/profile.entity.ts
@@ -1,7 +1,9 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
   @Column({
     type: 'varchar',
     length: 100,

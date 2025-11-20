b307fb0c76b1a84a1cb43a09240d6780c9ba806b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-21 00:33:02 +0300

    User Entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,23 @@
 //User Entity
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users') // ← renamed table
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
-
+  @Column({ type: 'varchar', length: 24, unique: true, nullable: false })
+  username: string;
   @Column({ type: 'varchar', length: 100, unique: true, nullable: false })
   email: string;
   @Column({ type: 'varchar', length: 100, nullable: false })
   password: string;
+  @CreateDateColumn() //Sets the value of the Created At Field when Created
+  createdAT: Date; //Record Date and Time when a User was Created.
+  updatedAT: Date;
+  deletedAT: Date;
 }
